aba3c81e7fd68fa42fbb502fc2f230cac19702ee
Fladson Machado <116230404+Fmachado17@users.noreply.github.com>
2022-11-24 01:18:08 +0000

    Dao completo

---
+++ database/Database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+class Database:
+  #Atributos
+  conexao = None
+  cursor = None
+
+#Método construtor
+def __init__(self):
+  self.conexao = sqlite3.connect("database/imdb.db")
+  self.cursor = self.conecxao.cursor()
+
+def __del__(self):
+ self.conexao.commit()
+  
+
+
